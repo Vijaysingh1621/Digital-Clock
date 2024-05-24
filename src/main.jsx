93b80944b0de349d63704b87212ad4612a99d5e8
Vijay Singh <rockcodingrookie21@gmail.com>
2024-05-24 16:58:49 +0530

    version-2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Digitalclock from './digital-clock-component/Digitalclock'
+import Digitalclock from './digitalClockComponent/Digitalclock'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <>
     <Digitalclock/>
+    </>
   </React.StrictMode>,
 )
