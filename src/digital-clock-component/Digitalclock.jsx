565e19f9b74462814cd9dc10e309dca43a503dfb
Vijay Singh <rockcodingrookie21@gmail.com>
2024-05-24 16:52:58 +0530

    version-1

---
+++ src/digital-clock-component/Digitalclock.jsx
@@ -0,0 +1,78 @@
+import React,{useState,useEffect} from 'react'
+import "./digitalclock.css"
+
+
+const Digitalclock = () => {
+const [time, setTime]=useState(new Date());
+
+useEffect(() =>{
+  const intervalId= setInterval(() => {
+    setTime(new Date());
+    
+  },1000);
+
+  return() => {
+    clearInterval(intervalId);
+  }
+  
+},[]);
+
+useEffect(() => {
+  const updateBackground = () => {
+    const hours = time.getHours();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    
+    let backgroundImage;
+    if (hours >= 5 && hours < 12 && meridiem === "AM") {
+      backgroundImage = "url(../../src/assets/morningclock.jpg)";
+    } else if (hours >= 12 && hours < 16 && meridiem === "PM") {
+      backgroundImage = "url(../../src/assets/noonclock.jpg)";
+    } else if (hours >= 16 && hours < 21 && meridiem === "PM") {
+      backgroundImage = "url(../../src/assets/eveningclock.jpg)";
+    } else {
+      backgroundImage = "url(../../src/assets/nightclock.jpg)";
+    }
+
+    document.body.style.backgroundImage = backgroundImage;
+  };
+  return()=>{
+
+  updateBackground();}
+}, [time]);
+
+function formatTime(){
+  let hours= time.getHours();
+  const minutes=time.getMinutes();
+  const seconds=time.getSeconds();
+  
+
+  hours =hours%12 || 12;
+
+  return `${padzero(hours)}:${padzero(minutes)}:${padzero(seconds)}`
+  
+}
+
+function formatMeridiem(){
+  let hours= time.getHours();
+  const meridiem= hours>=12? "PM":"AM" ;
+  return meridiem;
+}
+
+function padzero(number){
+  return (number<10 ? "0":"")+ number;
+}
+
+
+
+
+  return (
+    
+    <div className="clock-container" >
+        <div className="clock">
+            <span >{formatTime()}<p style={{fontSize:"4rem",display:'inline-block',paddingLeft:"20px"}}>{formatMeridiem()}</p></span>
+        </div>
+    </div>
+  )
+}
+
+export default Digitalclock
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Digitalclock from './digital-clock-component/Digitalclock'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Digitalclock/>
+  </React.StrictMode>,
+)
